cb5825d941330e2a2f877e9694e670c16107827e
VetoMrm <62132506+VetoMrm@users.noreply.github.com>
2020-06-26 17:42:30 +0530

    first commit

---
+++ Dist/Js/owl.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    $("#news-slider").owlCarousel({
+        items: 3,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [980, 2],
+        itemsMobile: [600, 1],
+        navigation: true,
+        navigationText: ["", ""],
+        pagination: true,
+        autoPlay: true
+    });
+});
+
+$(document).ready(function() {
+    $("#spotlightslide").owlCarousel({
+        items: 2,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [980, 2],
+        itemsMobile: [600, 1],
+        navigation: true,
+        navigationText: ["", ""],
+        pagination: true,
+        autoPlay: true
+    });
+});
+
+$(document).ready(function() {
+    $("#vetomobile").owlCarousel({
+        items: 1,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [980, 2],
+        itemsMobile: [600, 1],
+        navigation: true,
+        navigationText: ["", ""],
+        pagination: true,
+        autoPlay: true
+    });
+});
